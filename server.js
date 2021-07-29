5cf714e6eb81455b2dd8382d40299c32f3ac7066
Carlos Brito <cbrito1994@gmail.com>
2021-07-29 13:28:04 -0500

    required the routes and connected the server to localhost

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(morgan("dev"));
-
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutracker", {
@@ -18,3 +16,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutracker",
   useCreateIndex:true,
   useUnifiedTopology: true
 });
+
+app.use(require("./routes/paths"));
+app.use(require("./routes/api"));
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
