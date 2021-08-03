3d0e082e58a652b8fa58c49ded264501ce4c511c
Carlos Brito <cbrito1994@gmail.com>
2021-08-03 12:33:51 -0500

    deployment

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex:true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 app.use(require("./routes/paths.js"));
